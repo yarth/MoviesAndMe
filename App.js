5943df4ca9be12c5d92e839b5dba3e39a18a7c3e
Pascal Pommier <ppommier@akare.eu>
2019-02-23 00:03:18 +0100

    changement de titre

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Bienvenue dans l'application ReactNative de AKARE sarl</Text>
+        <Text>Bienvenue dans l'application ReactNative</Text>
       </View>
     );
   }
