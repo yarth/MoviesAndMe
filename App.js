4c28b7f64c975eeff277338bf249156f37abe9aa
Pascal Pommier <ppommier@akare.eu>
2019-02-23 12:32:31 +0100

    mise en place des styles

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Search from './Components/Search'
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Bienvenue dans l'application ReactNative</Text>
-      </View>
-    );
+      <Search/>
+    )
   }
 }
 
---
+++ Components/Search.js
@@ -0,0 +1,29 @@
+// Components/Search.js
+import React from 'react'
+import { StyleSheet, View, TextInput, Button } from 'react-native'
+
+class Search extends React.Component {
+  render() {
+    return (
+      <View style={{ marginTop: 100 }}>
+          <TextInput style={styles.textinput} placeholder='Titre du film'/>
+          <Button title='Rechercher' onPress={() => {}}/>
+      </View>
+    )
+  }
+}
+
+// Components/Search.js
+
+const styles = StyleSheet.create({
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: 'blue',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
+
+export default Search;
